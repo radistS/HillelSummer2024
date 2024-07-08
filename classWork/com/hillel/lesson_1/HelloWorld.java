3f74622302c5de44857f39ae94585d792f3f6f34
oleksandr.stepurko <stepurko.alexandr@gmail.com>
2024-07-08 19:51:12 +0200

    lesson_1: update first app

--- classWork/com/hillel/lesson_1/HelloWorld.java
+++ classWork/com/hillel/lesson_1/HelloWorld.java
@@ -3,5 +3,6 @@ package com.hillel.lesson_1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World! second line");
     }
 }
